1e776e1fecf64786219f6b47a0ab655c868833d9
Helder Pinto <helder.manuel.afonso.pinto@sap.com>
2020-06-17 20:25:41 +0200

    Updates

---
+++ precache-manifest.e493a412f4bae1d7a2aa559020905492.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b5205ecb4c8c5e300bc4b6745abfec0e",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "e91c5a74875b048f3f89",
+    "url": "/monsters-rolodex/static/css/main.e3836662.chunk.css"
+  },
+  {
+    "revision": "80de6e261c3a1f36230c",
+    "url": "/monsters-rolodex/static/js/2.05463ef1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.05463ef1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e91c5a74875b048f3f89",
+    "url": "/monsters-rolodex/static/js/main.a29c6b35.chunk.js"
+  },
+  {
+    "revision": "647f06759b25837e4d32",
+    "url": "/monsters-rolodex/static/js/runtime-main.d3ed8291.js"
+  }
+]);
